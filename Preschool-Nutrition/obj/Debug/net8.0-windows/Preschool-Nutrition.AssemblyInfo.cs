6183c4b05a9bcfa253052e9b78c8bd17c281adfb
BaoBao <dinhb140@gmail.com>
2024-09-23 22:57:25 +0700

    update views

--- Preschool-Nutrition/obj/Debug/net8.0-windows/Preschool-Nutrition.AssemblyInfo.cs
+++ Preschool-Nutrition/obj/Debug/net8.0-windows/Preschool-Nutrition.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Preschool-Nutrition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb38772c57ea6064470c49e213d014ef3ec6b757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017f23a28e0984217db05523d0d18a62f15c573e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Preschool-Nutrition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Preschool-Nutrition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
