1669df9c5d95200265f727ad449fd4c3b971e851
BaoBao <dinhb140@gmail.com>
2024-10-05 18:12:22 +0700

    fix MonAnReponsitory

--- Preschool-Nutrition/Repositories/MonAnRepository.cs
+++ Preschool-Nutrition/Repositories/MonAnRepository.cs
@@ -11,32 +11,6 @@ namespace Preschool_Nutrition.Repositories
 {
     public class MonAnRepository
     {
-        public List<MonAn> GetAllMonAn()
-        {
-            List<MonAn> monAnList = new List<MonAn>();
-            using (var connection = DatabaseHelper.GetConnection())
-            {
-                string query = "SELECT * FROM MonAn";
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            MonAn monAn = new MonAn
-                            {
-                                MaMonAn = reader["MaMonAn"].ToString(),
-                                TenMonAn = reader["TenMonAn"].ToString(),
-                                LoaiMonAn = reader["LoaiMonAn"].ToString(),
-                                Calo = Convert.ToSingle(reader["Calo"]),
-                                GhiChu = reader["GhiChu"].ToString()
-                            };
-                            monAnList.Add(monAn);
-                        }
-                    }
-                }
-            }
-            return monAnList;
-        }
+
     }
 }
